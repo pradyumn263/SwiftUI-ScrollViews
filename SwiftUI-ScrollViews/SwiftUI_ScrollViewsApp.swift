0232722040daf3ef5fa78e11ecbea6528136ba4b
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-04 15:59:56 +0530

    Initial Commit

---
+++ SwiftUI-ScrollViews/SwiftUI_ScrollViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ScrollViewsApp.swift
+//  SwiftUI-ScrollViews
+//
+//  Created by Pradyumn Shukla on 04/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ScrollViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
